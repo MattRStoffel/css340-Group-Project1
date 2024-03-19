44a599792f45601d3a3dc2fa9a1ccbafd16eb459
Matt <MattRStoffel@gmail.com>
2024-03-19 14:09:46 -0700

    Moved implementation of fnuctions from .h to .cpp

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -145,7 +145,7 @@ namespace UNITTESTS {
     bool test_generateRandomIntegers_1() {
         std::vector<int> testData;
         testData = RandomGenerators::generateRandomNormalDistInts(20000, 0, 1);
-        return Statistics::isApproximatelyNormal(testData);
+        return isApproximatelyNormal(testData);
     }
 
     bool test_generateRandomIntegers_2() {
@@ -184,7 +184,7 @@ namespace UNITTESTS {
     bool test_generateRandomFloats_1() {
         std::vector<float> testData;
         testData = RandomGenerators::generateRandomUniformDistFloats(20000, 10, 100);
-        return Statistics::isApproximatelyUniform(testData);
+        return isApproximatelyUniform(testData);
     }
 
     bool test_generateRandomFloats_2() {
@@ -220,4 +220,81 @@ namespace UNITTESTS {
         return false;
     }
 
-}
+        //function to check if a value is numerical
+    template<typename T>
+    constexpr bool is_numerical() {
+        return std::is_arithmetic<T>::value;
+    }
+
+    //function checks validity of input data
+    template<typename T>
+    void validVectorDataCheck(const std::vector<T>& data) {
+        if (data.empty()) {
+            throw std::runtime_error("Error: Input vector is empty.");
+        }
+
+        if (!is_numerical<T>()) {
+            throw std::runtime_error("Error: Input vector contains non-numerical values.");
+        }
+
+    }
+
+    //function calculates the mean of a collection of vector elements
+    template<typename T>
+    double calculateMean(const std::vector<T>& data) {
+        validVectorDataCheck(data);
+        return std::accumulate(data.begin(), data.end(), 0.0) / data.size();
+    }
+
+    //function calculated the median of a collection of vector elements
+    template<typename T>
+    double calculateMedian(std::vector<T> data) {
+        validVectorDataCheck(data);
+
+        size_t size = data.size();
+        std::sort(data.begin(), data.end());
+        if (size % 2 == 0) {
+            return (data[size / 2 - 1] + data[size / 2]) / 2.0;
+        } else {
+            return data[size / 2];
+        }
+    }
+
+    //function calculates if a collection of vector elements is approx. normal
+    //determined by comparing the mean and median.
+    //if the two values are approx. equivalent (within specified) threshold,
+    //the distribution is considered normal
+    template<typename T>
+    bool isApproximatelyNormal(const std::vector<T>& data, double allowableDifference) {
+        validVectorDataCheck(data);
+        double mean = calculateMean(data);
+        double median = calculateMedian(data);
+        return std::abs(mean - median) < allowableDifference;
+    }
+
+    //function calculates if a collection of vector elements is approx. uniform
+    //determined by checking if the mean and median are roughly the same value as the midpoint of the range
+    //if the two values are approximately equivalent (within specified) threshold,
+    //the distribution is considered uniform
+    template<typename T>
+    bool isApproximatelyUniform(const std::vector<T>& data, double allowableDifference) {
+        validVectorDataCheck(data);
+
+        auto lowerBound = *std::min_element(data.begin(), data.end());
+        auto upperBound = *std::max_element(data.begin(), data.end());
+
+        T rangeMidPoint = (upperBound + lowerBound) / 2;
+        double mean = calculateMean(data);
+        double median = calculateMedian(data);
+
+        // Check if the absolute difference between mean and rangeMidPoint is within allowableDifference
+        bool meanCheck = std::abs(mean - rangeMidPoint) < allowableDifference;
+        // Check if the absolute difference between median and rangeMidPoint is within allowableDifference
+        bool medianCheck = std::abs(median - rangeMidPoint) < allowableDifference;
+
+        // Return true if both checks pass
+        return meanCheck && medianCheck;
+    }
+
+} // namespace UNITTESTS
+
--- UnitTests.h
+++ UnitTests.h
@@ -42,86 +42,26 @@ namespace UNITTESTS {
     bool test_quickSort_1();
     bool test_quickSort_2();
     bool test_quickSort_3();
-}
 
-namespace Statistics {
-
-    //function to check if a value is numerical
     template<typename T>
-    constexpr bool is_numerical() {
-        return std::is_arithmetic<T>::value;
-    }
+    constexpr bool is_numerical();
 
-    //function checks validity of input data
     template<typename T>
-    void validVectorDataCheck(const std::vector<T>& data) {
-        if (data.empty()) {
-            throw std::runtime_error("Error: Input vector is empty.");
-        }
+    void validVectorDataCheck(const std::vector<T>& data);
 
-        if (!is_numerical<T>()) {
-            throw std::runtime_error("Error: Input vector contains non-numerical values.");
-        }
-
-    }
-
-    //function calculates the mean of a collection of vector elements
     template<typename T>
-    double calculateMean(const std::vector<T>& data) {
-        validVectorDataCheck(data);
-        return std::accumulate(data.begin(), data.end(), 0.0) / data.size();
-    }
+    double calculateMean(const std::vector<T>& data);
 
-    //function calculated the median of a collection of vector elements
     template<typename T>
-    double calculateMedian(std::vector<T> data) {
-        validVectorDataCheck(data);
+    double calculateMedian(std::vector<T> data);
 
-        size_t size = data.size();
-        std::sort(data.begin(), data.end());
-        if (size % 2 == 0) {
-            return (data[size / 2 - 1] + data[size / 2]) / 2.0;
-        } else {
-            return data[size / 2];
-        }
-    }
-
-    //function calculates if a collection of vector elements is approx. normal
-    //determined by comparing the mean and median.
-    //if the two values are approx. equivalent (within specified) threshold,
-    //the distribution is considered normal
     template<typename T>
-    bool isApproximatelyNormal(const std::vector<T>& data, double allowableDifference = 1e-2) {
-        validVectorDataCheck(data);
-        double mean = calculateMean(data);
-        double median = calculateMedian(data);
-        return std::abs(mean - median) < allowableDifference;
-    }
+    bool isApproximatelyNormal(const std::vector<T>& data, double allowableDifference = 1e-2);
 
-    //function calculates if a collection of vector elements is approx. uniform
-    //determined by checking if the mean and median are roughly the same value as the midpoint of the range
-    //if the two values are approximately equivalent (within specified) threshold,
-    //the distribution is considered uniform
     template<typename T>
-    bool isApproximatelyUniform(const std::vector<T>& data, double allowableDifference = 0.5) {
-        validVectorDataCheck(data);
+    bool isApproximatelyUniform(const std::vector<T>& data, double allowableDifference = 0.5);
 
-        auto lowerBound = *std::min_element(data.begin(), data.end());
-        auto upperBound = *std::max_element(data.begin(), data.end());
 
-        T rangeMidPoint = (upperBound + lowerBound) / 2;
-        double mean = calculateMean(data);
-        double median = calculateMedian(data);
-
-        // Check if the absolute difference between mean and rangeMidPoint is within allowableDifference
-        bool meanCheck = std::abs(mean - rangeMidPoint) < allowableDifference;
-        // Check if the absolute difference between median and rangeMidPoint is within allowableDifference
-        bool medianCheck = std::abs(median - rangeMidPoint) < allowableDifference;
-
-        // Return true if both checks pass
-        return meanCheck && medianCheck;
-    }
-
-} // namespace Statistics
+} // namespace UNITTESTS
 
 #endif // UNITTESTS_H
